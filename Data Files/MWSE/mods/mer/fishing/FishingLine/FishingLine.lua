000a91fc54791164c665de1b58d5e63956681f08
Greatness7 <Greatness7@gmail.com>
2023-05-16 06:36:12 -0400

    Maybe improved fishing line lure tracking.

--- Data Files/MWSE/mods/mer/fishing/FishingLine/FishingLine.lua
+++ Data Files/MWSE/mods/mer/fishing/FishingLine/FishingLine.lua
@@ -101,7 +101,7 @@ function FishingLine:updateEndPoints(origin, destination)
     endp.value = position * 2
     endp.value.z = 0
     self.curveData:updateDerivedValues()
-    self.sceneNode:update()
+    self.sceneNode:update({controllers=true})
 end
 
 return FishingLine
