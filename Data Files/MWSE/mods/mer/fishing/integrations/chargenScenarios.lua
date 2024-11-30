494d64f40b13ae36ecee67c35c5e9189f853354e
jhaakma <jchaakma@gmail.com>
2024-11-30 21:56:28 +1300

    Add Chargen Scenarios interop

---
+++ Data Files/MWSE/mods/mer/fishing/integrations/chargenScenarios.lua
@@ -0,0 +1,39 @@
+local ChargenScenarios = include("mer.chargenScenarios")
+
+---@type ChargenScenarios.ItemListInput[]
+local loadouts = {
+    {
+        name = "Fishing Gear",
+        description = "Some basic fishing gear.",
+        items = {
+            { id = "misc_de_fishing_pole" },
+            { id = "misc_com_basket_02" },
+            {
+                description = "Fishing Lure",
+                ids = {
+                    "mer_bug_spinner",
+                    "mer_bug_spinner2",
+                    "mer_silver_lure"
+                }
+            },
+            {
+                description = "Bait",
+                ids = {
+                    "mer_meat_seabass",
+                    "mer_meat_cod",
+                    "mer_meat_tambaqui"
+                },
+                count = 3
+            },
+        }
+    },
+}
+
+event.register("initialized", function (e)
+    for _, loadout in ipairs(loadouts) do
+        ChargenScenarios.registerLoadout{
+            id = "ultimateFishing:" .. loadout.name,
+            itemList = loadout
+        }
+    end
+end)
